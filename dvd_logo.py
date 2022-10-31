032cdaf95d30693706020a3ed4980f293e6156d3
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-10-30 23:39:15 -0400

    Important Visual Changes!
    Added a white transparent DVD logo so the background can be black. (good for OLED screens!)
    
    Also added a walls & corner counter.
    
    The only problem is that the logo has to hit both walls on the EXACT SAME FRAME. Otherwise it's not technically a corner hit. Which might not be satisfying for some users. But technically speaking that's a true corner hit.
    
    Feel free to improve that to add more leniency.
    
    Also one other thing I want to add at some point is the ability to mute the beeping sound through a key press and maybe pause the dvd logo at a specific key press too.
    
    Might also be nice if you could increase the speed of the dvd logo bouncing on a scale of 1 to 10 or something.
    
    Those should be easy to add in but I've not got time right now.

--- dvd_logo.py
+++ dvd_logo.py
@@ -14,19 +14,24 @@ def main(self):
     SCREEN = self.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     self.display.set_caption("DVD Logo Screensaver")
 
-    bg_color = (0, 255, 255)
+    font = pygame.font.Font('freesansbold.ttf', 14)
+
+    bg_color = (0, 0, 0)
 
     chosen_speed = int(random.randint(3, 6))
 
     initial_speed_x = chosen_speed
     initial_speed_y = chosen_speed
 
-    dvd_logo_image = self.image.load('dvd_logo_transparent.png')
+    dvd_logo_image = self.image.load('dvd_logo_transparent_white_small.png')
     image_rect = dvd_logo_image.get_rect()
 
     wall_hit_sound = pygame.mixer.Sound('wall_hit_beep_sound.wav')
 
     mixer.music.set_volume(1)
+
+    walls_hit_counter = 0
+    corners_hit_counter = 0
 
     while True:
 
@@ -38,17 +43,41 @@ def main(self):
         image_rect.x += initial_speed_x
         image_rect.y += initial_speed_y
 
-        if image_rect.top <= 0 or image_rect.bottom >= SCREEN_HEIGHT:
+        if (image_rect.top <= 0 or image_rect.bottom >= SCREEN_HEIGHT) and (
+                image_rect.left <= 0 or image_rect.right >= SCREEN_WIDTH):
             initial_speed_y *= -1
-            pygame.mixer.Sound.play(wall_hit_sound).set_volume(0.15)
-
-        if image_rect.left <= 0 or image_rect.right >= SCREEN_WIDTH:
             initial_speed_x *= -1
             pygame.mixer.Sound.play(wall_hit_sound).set_volume(0.15)
+            corners_hit_counter += 1
+
+        elif image_rect.top <= 0 or image_rect.bottom >= SCREEN_HEIGHT:
+            initial_speed_y *= -1
+            pygame.mixer.Sound.play(wall_hit_sound).set_volume(0.15)
+            walls_hit_counter += 1
+
+        elif image_rect.left <= 0 or image_rect.right >= SCREEN_WIDTH:
+            initial_speed_x *= -1
+            pygame.mixer.Sound.play(wall_hit_sound).set_volume(0.15)
+            walls_hit_counter += 1
+
 
         # visual stuff
         SCREEN.fill(bg_color)
         SCREEN.blit(dvd_logo_image, image_rect)
+        walls_hit_counter_text = font.render(f"Walls hit: {walls_hit_counter}", True,
+                                             (255, 255, 255), (0, 0, 0))
+
+        wall_counter_rect = walls_hit_counter_text.get_rect()
+
+        display_wall_counter = SCREEN.blit(walls_hit_counter_text, wall_counter_rect)
+
+        corners_hit_counter_text = font.render(f"Corners hit: {corners_hit_counter}", True,
+                                               (255, 255, 255), (0, 0, 0))
+
+        corner_counter_rect = corners_hit_counter_text.get_rect().move(0, 24)
+
+        display_corner_counter = SCREEN.blit(corners_hit_counter_text, corner_counter_rect)
+
 
         self.display.flip()
         clock.tick(60)
